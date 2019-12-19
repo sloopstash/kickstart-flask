27ed3490dd4cda7c0911e79bd78bef06458cf904
Sathiyaraj Sridhar <sathiyas20@gmail.com>
2019-12-19 20:05:10 +0530

    Modified deployment scripts for AWS CodeDeploy service; Used sed command to replace default credentials of the app and redis configuration.

--- script/deploy/configure.sh
+++ script/deploy/configure.sh
@@ -8,3 +8,12 @@ cp /opt/app/config/supervisor/main.conf /etc/supervisord.conf
 
 # Add supervisor configuration for app.
 cp /opt/app/config/supervisor/app.ini /etc/supervisord.d/app.ini
+
+DEFAULT_REDIS_HOST="crm-redis"
+REDIS_HOST=$(aws ssm get-parameter --name CRM-PRD-Redis-Host --query Parameter.Value --region us-west-2)
+sed -i 's|"'$DEFAULT_REDIS_HOST'"|'$REDIS_HOST'|g' /opt/app/config/redis.conf
+
+DEFAULT_STATIC_ENDPOINT="http://app.crm"
+STATIC_ENDPOINT=$(aws ssm get-parameter --name CRM-PRD-STT-END --query Parameter.Value --region us-west-2)
+sed -i 's|"'$DEFAULT_STATIC_ENDPOINT'"|'$STATIC_ENDPOINT'|g' /opt/app/config/app.conf
+sed -i 's|"dev"|"prd"|g' /opt/app/config/app.conf
