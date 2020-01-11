a3fa20bba6ca04ac9555b0b37b5d45607514fa02
John Sundarraj <johnsundarraj88@gmail.com>
2020-01-11 18:38:59 +0530

    Health check url output modified due to issue in Jenkins.

--- script/test/main.py
+++ script/test/main.py
@@ -21,7 +21,7 @@ def client():
 def test_health_check(client):
   response = client.get('/health')
   assert response.status_code == 200
-  assert response.data == 'Healthy'
+  assert response.data == 'OK'
   print "Test : Health check - passed"
 
 # Test dashboard view.
