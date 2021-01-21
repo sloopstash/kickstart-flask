63f4a9b81db23ce4561de3a1e0bdba1923d6ba6f
Sathiyaraj Sridhar <sathiyas20@gmail.com>
2021-01-21 13:13:41 +0530

    Modified SSM parameter names; Removed unwanted snippets;

--- script/deploy/configure.sh
+++ script/deploy/configure.sh
@@ -3,17 +3,14 @@
 # Install required packages for app.
 pip install -r /opt/app/requirements.txt
 
-# Modify supervisor configuration.
-cp /opt/app/config/supervisor/main.conf /etc/supervisord.conf
-
 # Add supervisor configuration for app.
 cp /opt/app/config/supervisor/app.ini /etc/supervisord.d/app.ini
 
 DEFAULT_REDIS_HOST="crm-redis"
-REDIS_HOST=$(aws ssm get-parameter --name CRM-PRD-Redis-Host --query Parameter.Value --region us-west-2)
+REDIS_HOST=$(aws ssm get-parameter --name PRD-EC2-Redis-SR-1 --query Parameter.Value --region us-west-2)
 sed -i 's|"'$DEFAULT_REDIS_HOST'"|'$REDIS_HOST'|g' /opt/app/config/redis.conf
 
 DEFAULT_STATIC_ENDPOINT="http://app.crm"
-STATIC_ENDPOINT=$(aws ssm get-parameter --name CRM-PRD-STT-END --query Parameter.Value --region us-west-2)
+STATIC_ENDPOINT=$(aws ssm get-parameter --name PRD-CF-STT-DST --query Parameter.Value --region us-west-2)
 sed -i 's|"'$DEFAULT_STATIC_ENDPOINT'"|'$STATIC_ENDPOINT'|g' /opt/app/config/app.conf
 sed -i 's|"dev"|"prd"|g' /opt/app/config/app.conf
--- script/deploy/setup.sh
+++ script/deploy/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install system packages.
-yum -y update
-yum install -y ruby git python-devel python-setuptools wget vim net-tools initscripts gcc make tar bind-utils nc
-
 # Make app directory.
 APP_DIR=/opt/app
 if [ -d "$APP_DIR" ]; then
